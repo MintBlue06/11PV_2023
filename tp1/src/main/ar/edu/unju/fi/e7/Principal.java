3778fc0c5744259b04c1df4620cd5019d75eb237
MintBlue06 <p1210mizu@gmail.com>
2023-08-09 16:06:05 -0300

    Commit 9/8: *En proceso e7

--- tp1/src/main/ar/edu/unju/fi/e7/Principal.java
+++ tp1/src/main/ar/edu/unju/fi/e7/Principal.java
@@ -1,7 +1,72 @@
 package main.ar.edu.unju.fi.e7;
+//Falta terminar
+import java.util.Scanner;
 
 public class Principal {
+
+    private static int numero;
+
+    private static int factorial;
     public static void main(String[] args) {
         
+        Principal principal = new Principal();
+
+        principal.conseguirNumero();
+
+        principal.factorialDelNumero( numero);
+
+        /*for (int i = 0; i < 10; i++) {
+            
+            System.out.printf("Factorial de %d: . Iterativo: %d", i, factorial);
+            System.out.println(".()");
+        }*/
+        System.out.println("Factorial de " + numero + ": "+ factorial);
+    }
+    public void conseguirNumero() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+
+        numero = scanner.nextInt();
+
+        scanner.close();
+    }
+
+    public int factorialDelNumero(int numero) {
+
+        //if (numero <= 0) {
+
+           
+        //}
+
+        //return 1;
+
+        if (numero <= 0) {
+
+            if (numero < 0) {}
+
+            numero *= -1;
+            //return 1;
+
+       } else {
+
+        return 1;
+        //numero *= -1;
+        }
+
+        
+        //numero *= -1;
+        
+        factorial = 1;
+        while (numero > 1) {
+            
+            factorial *= numero;
+
+            numero--;
+
+        }
+        return factorial;
+
     }
 }
