5b5031f643807d3bc11da14dd0982d8e0ed5c6f9
MintBlue06 <p1210mizu@gmail.com>
2023-12-19 20:55:42 -0300

    Ejercicio e8 completo
    agregar ejercicio e9

--- tp1/src/main/ar/edu/unju/fi/e8/Principal.java
+++ tp1/src/main/ar/edu/unju/fi/e8/Principal.java
@@ -1,10 +1,22 @@
 package main.ar.edu.unju.fi.e8;
 
 import java.util.Scanner;
-
+/**
+ * @author Alvarez Carlos Matias
+ * @since 19/12/23
+ * 
+ * Se solicita nombre y edad, posteriormente
+ *  se verifica si es mayor o menor de edad
+ */
 public class Principal {
+    private static String nombre;
+    private static byte edad;
     public static void main(String[] args) {
-        
+
+        Principal principal = new Principal();
+        principal.conseguirDatos();
+        principal.mensaje(nombre, edad);
+        principal.verificarSiEsMayorDeEdad(edad);
     }
 
     public void conseguirDatos() {
@@ -13,14 +25,22 @@ public class Principal {
 
         System.out.println("Ingresar nombre");
 
-        String nombre = scanner.nextLine();
+        nombre = scanner.nextLine();
 
         System.out.println("Ingresar edad");
 
-        byte edad = scanner.nextByte();
+        edad = scanner.nextByte();
 
         scanner.close();
     }
-
-    public void verificarSiEsMayorDeEdad() {}
+    public void mensaje(String nombre, byte edad) {
+        System.out.println("El usuario " + nombre + " con edad " + edad + "");
+    }
+    public void verificarSiEsMayorDeEdad(byte edad) {
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    }
 }
---
+++ tp1/src/main/ar/edu/unju/fi/e9/Principal.java
@@ -0,0 +1,10 @@
+package main.ar.edu.unju.fi.e9;
+/**
+ * @author Alvarez Carlos Matias
+ * @since 19/12/23
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
